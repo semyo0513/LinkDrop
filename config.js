93263367a0b01430f52bbba18afd6a08c68abd3a
semyo0513 <semyo0513@users.noreply.github.com>
2026-06-25 14:49:49 +0900

    deploy: update GAS URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Google Apps Script Web App URL 설정 파일
 // 이 파일은 deploy.js 실행 시 자동으로 업데이트됩니다.
 window.CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbxJf8eLbYJK-o68EzP4GWRqe2QKWJUkxWx-IKrUZMnJAHKWELnuM3pLxqE3dSoC-_2LEw/exec"
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzvaRxWzVtv5p3WLNJrD_i1lNH2K9mmptIGIr6A-ammxHDvpzOeWTSVmPlQAUqOh3pb0Q/exec"
 };
